fa510a914383d871ee457af8384f1520bfaaf372
rochelleerfe <erferochelle0104@gmail.com>
2025-08-09 23:20:45 +0800

    Update Calculator.dart

--- Calculator.dart
+++ Calculator.dart
@@ -6,27 +6,34 @@ void main() {
   int num2 = 72;
 
   // Display the numbers
-  print('Number 1: $num1');
-  print('Number 2: $num2\n');
+  print('First Number: $num1');
+  print('Second Number: $num2\n');
 
-  //naka display po dito yung mga calculation 
-  int addition = num1 + num2;
-  int subtraction = num1 - num2;
-  int multiplication = num1 * num2;
-  double division = num1 / num2;
+  // Perform the calculations
+  num add() => num1 + num2;
+  num subtract() => num1 - num2;
+  num multiply() => num1 * num2;
+  num divide() => num1 / num2;
 
-  print('Addition: $num1 + $num2 = $addition');
-  print('Division: $num1 / $num2 = $division\n');
-  print('Subtraction: $num1 - $num2 = $subtraction');
-  print('Multiplication: $num1 * $num2 = $multiplication');
+  // Output results
+  print('Addition: $num1 + $num2 = ${add()}');
+  print('Division: $num1 / $num2 = ${divide()}\n');
+  print('Subtraction: $num1 - $num2 = ${subtract()}');
+  print('Multiplication: $num1 * $num2 = ${multiply()}');
 }
 
-// expected result
 
-// Number 1: 8
-// Number 2: 72
+// Expected output
+// Any numbers can be calculated
+
+// Simple Calculator 
+
+// First Number: 8
+// Second Number: 72
 
 // Addition: 8 + 72 = 80
 // Division: 8 / 72 = 0.1111111111111111
+
 // Subtraction: 8 - 72 = -64
 // Multiplication: 8 * 72 = 576
+
